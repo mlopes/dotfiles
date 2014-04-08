5e1d6f7618aa9c0209b0797c6ceaef441dc0f60d
Marco Lopes <marco.paulo.lopes@gmail.com>
2014-04-08 15:38:48 +0100

    Replace keychain with envoy

--- .config/fish/functions/ssh.fish
+++ .config/fish/functions/ssh.fish
@@ -1,6 +1,5 @@
 function ssh
-    set -xg SHELL fish; set -xg ssh_env (/usr/bin/keychain --eval --agents ssh -Q --quiet ~/.ssh/id_rsa)
-    set -xg ssh_env (echo $ssh_env | sed 's/ set -e/; set -e/')                                         
-    eval $ssh_env          
+    envoy -t ssh-agent
+    eval (envoy -pf | sed 's/-x/-x -U/')  
     command ssh $argv
 end
