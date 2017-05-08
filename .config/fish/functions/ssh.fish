db878c54d5338a96f5e7817de41e61e527646fb5
Marco Lopes <marco.paulo.lopes@gmail.com>
2017-05-08 15:01:04 +0100

    Comment out envoy from fish's ssh scripts

--- .config/fish/functions/ssh.fish
+++ .config/fish/functions/ssh.fish
@@ -1,5 +1,5 @@
-function ssh
-    envoy -t ssh-agent
-    eval (envoy -pf | sed 's/-x/-x -U/')  
-    command ssh $argv
-end
+# function ssh
+#    envoy -t ssh-agent
+#    eval (envoy -pf | sed 's/-x/-x -U/')  
+#    command ssh $argv
+# end
