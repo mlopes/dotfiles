d82ba5364862d576fb7643764408c429df1e533e
Marco Lopes <marco.paulo.lopes@gmail.com>
2017-08-03 10:11:13 +0100

    Improve xmobar configuration

--- .xmonad/xmobar.hs
+++ .xmonad/xmobar.hs
@@ -5,18 +5,21 @@ Config {
     -- fgColor = "#009C00",
     bgColor = "#000000",
     fgColor = "#FFAF00",
+    alpha = 150,
     border = TopB,
     borderWidth = 1,
     borderColor = "#FF6600",
     pickBroadest = True
-    position = Top
+    position = Top,
+    textOffset = 16,
     lowerOnStart = False,
     commands = [
+        Run Locks,
         --Run Weather "EGLL" ["-t","<tempC>C <skyCondition>","-L","0","-H","25","-n","#ffca28","-h","#FFB6B0","-l","#96CBFE"] 36000,
-        Run Weather "EGLL" ["-t","<fn=1>\xf0c2</fn> <tempC>C <skyCondition>","-L","10","-H","25","-n","#ffca28","-h","#FFB6B0","-l","#96CBFE"] 3600,
-        Run MultiCpu ["-t","<fn=1>\xf085</fn><total0><total1><total2><total3>","-L","30","-H","60","-h","#FFB6B0","-l","#ffca28","-n","#FFFFCC","-w","3"] 10,
+        Run Weather "EGLL" ["-t","<fn=1>\xf0c2</fn> <tempC>°<skyCondition> ","-L","10","-H","25","-n","#ffca28","-h","#FFB6B0","-l","#96CBFE"] 3600,
+        Run MultiCpu ["-t","<fn=1>\xf0e4</fn><total0> <vbar0><total1> <vbar1><total2> <vbar2><total3> <vbar3>","-L","30","-H","60","-h","#FFB6B0","-l","#ffca28","-n","#FFFFCC","-w","3"] 10,
         Run Memory ["-t","<fn=1>\xf2db</fn> <usedratio>%","--High","80","--Low","10","--high","#FF6600","--low","#96CBFE","--normal","#ffca28"] 10,
-        Run Swap ["-t","<usedratio>% swap","-H","1024","-L","512","-h","#FFB6B0","-l","#ffca28","-n","#FFFFCC"] 10,
+        Run Swap ["-t","<fn=1>\xf019</fn> <usedratio>%","-H","1024","-L","512","-h","#FFB6B0","-l","#ffca28","-n","#FFFFCC"] 10,
         Run Network "enp2s0f0" ["-t","Net: <rx>, <tx>","-H","200","-L","10","-h","#FFB6B0","-l","#ffca28","-n","#FFFFCC"] 10,
         Run Wireless "wlp3s0" [ "-t", "<fn=1>\xf1eb</fn> <fc=#ffca28><essid></fc> [<qualitybar>]","-L","0","-H","32","--normal","yellow","--high","#ffca28","--low","red"] 10,
         Run Date "<fn=1>\xf073</fn> <fc=#ffca28>%a %b %d %H:%M</fc>" "date" 10,
@@ -30,8 +33,9 @@ Config {
             , "-n", "#b58900"
             , "-h", "orange"
             , "--" -- battery specific options
+            , "-p", "green"
             -- discharging status
-            , "-o"  , "<fn=1>\xf242</fn> <left>% (<timeleft>) <watts>"
+            , "-o"  , "<fn=1>\xf242</fn> <left>% <leftvbar> (<timeleft>)"
 			, "-m", "#ffca28"
             -- AC "on" status
             , "-O"  , "<fn=1>\xf1e6</fn> <left>%"
@@ -47,11 +51,9 @@ Config {
                                   , "--off", "<fc=orange><fn=1>\xf026</fn> MUTE</fc>"
                                   , "--offc", "#dc322f"
                                    ] 1,
-        -- Run Com "/home/mlopes/.xmonad/bin/wifi.sh" [] "wifi" 30,
-       Run Com "/home/mlopes/.xmonad/bin/volume.sh" [] "volume" 3,
        Run StdinReader
     ],
     sepChar = "%",
     alignSep = "}{",
-    template = "%StdinReader% }{%multicpu% %memory% %swap%  %wlp3s0wi% %default:Master% %battery% %date% %EGLL%"
+    template = "%StdinReader% }{%locks% %multicpu% %memory% %swap% %wlp3s0wi% %default:Master% %battery% %date% %EGLL%"
 }
