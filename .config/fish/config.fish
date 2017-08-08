2c303253ac49c36670986addb95df81b40bea912
Marco Lopes <marco.paulo.lopes@gmail.com>
2017-08-08 09:33:06 +0100

    Re-theme prompt

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,13 +4,20 @@ if status --is-interactive
  keychain --eval --quiet -Q id_rsa | source
 end
 
+set __prompt_colour '#FFCA28'
+set __prompt_text_colour '#FAB511'
+set __prompt_date_colour '#FCD984'
+
 # Fish git prompt
 set __fish_git_prompt_showdirtystate 'yes'
 set __fish_git_prompt_showstashstate 'yes'
 set __fish_git_prompt_showupstream 'yes'
-set __fish_git_prompt_color_branch yellow
-set __fish_git_prompt_color_suffix blue
+set __fish_git_prompt_color_branch white
+set __fish_git_prompt_color_suffix '#B511FA'
+set __fish_git_prompt_color_prefix '#B511FA'
 set __fish_git_prompt_showuntrackedfiles 'yes'
+set __fish_git_prompt_color_flags '#FCD984'
+set __fish_git_prompt_color_merging '#FAB511'
 
 # Status Chars
 set __fish_git_prompt_char_dirtystate '⚡'
@@ -18,6 +25,7 @@ set __fish_git_prompt_char_stagedstate '→'
 set __fish_git_prompt_char_stashstate '↩'
 set __fish_git_prompt_char_upstream_ahead '↑'
 set __fish_git_prompt_char_upstream_behind '↓'
+set __fish_git_prompt_char_untrackedfiles '…'
 set __fish_git_prompt_char_upstream_equal ''
 
 #set PATH $HOME/.rbenv/bin $PATH
