609f075e1d16044a10db471fd37faf165f3c083a
Marco Lopes <marco.paulo.lopes@gmail.com>
2017-08-20 20:45:10 +0100

    Change font sizes to match the xps13 resolution

--- .xmonad/xmobar.hs
+++ .xmonad/xmobar.hs
@@ -1,6 +1,6 @@
 Config {
-    font = "xft:Source\ Code\ Pro\ for\ Powerline-14,FontAwesome:size=15",
-    additionalFonts   = [ "xft:FontAwesome:pixelsize=15:antialias=true:hinting=true" ],
+    font = "xft:Source\ Code\ Pro\ for\ Powerline-10,FontAwesome:size=10",
+    additionalFonts   = [ "xft:FontAwesome:pixelsize=20:antialias=true:hinting=true" ],
     -- bgColor = "#000900",
     -- fgColor = "#009C00",
     bgColor = "#330022",
