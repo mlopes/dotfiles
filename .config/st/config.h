5a620ede4344b348144dcb2488fb2414d00aea63
Marco Lopes <marco.paulo.lopes@gmail.com>
2017-09-06 11:17:24 +0100

    Update st config

--- .config/st/config.h
+++ .config/st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char font[] = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char font[] = "DejaVu Sans Mono for Powerline:size=7:antialias=true:autohint=true";
 static int borderpx = 2;
 #define histsize 20000
 
