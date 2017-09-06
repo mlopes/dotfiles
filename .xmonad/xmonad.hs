a845c24029bcc0a00399e34d5aa4998fcb915091
Marco Lopes <marco.paulo.lopes@gmail.com>
2017-09-06 10:58:01 +0100

    Attempt to use st as default terminal

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -42,7 +42,8 @@ import XMonad.Util.Run (runProcessWithInput)
 --
 -- myTerminal = "urxvt -e fish -c \"tmux -q has-session; and exec tmux attach-session -d; or exec tmux new-session -n$USER -s$USER@$HOSTNAME\""
 -- myTerminal = "/usr/bin/urxvt +ls -e fish -l"
-myTerminal = "/usr/bin/urxvt +ls -e fish -l"
+-- myTerminal = "/usr/bin/urxvt +ls -e fish -l"
+myTerminal = "st -f 'DejaVu Sans Mono:size=7:antialias=true:autohint=true' -e fish -l -c ~/.local/bin/terminal"
 
 
 ------------------------------------------------------------------------
