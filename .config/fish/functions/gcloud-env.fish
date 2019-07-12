e12e8d4854c793249ece13a316cf59bdee36db84
Marco Lopes <marco.paulo.lopes@gmail.com>
2019-07-12 10:25:25 +0100

    Update gcloud fish script

--- .config/fish/functions/gcloud-env.fish
+++ .config/fish/functions/gcloud-env.fish
@@ -1,3 +1,3 @@
 function gcloud-env
-  gcloud container clusters get-credentials $argv[1]-kubernetes-01 --zone europe-west2-a --project $argv[1];
+  gcloud --project $argv[1] container clusters get-credentials paygplus --region europe-west1;
 end
