658337ba1580f1c1df6b0bba24adbb0aba5c8bdc
Marco Lopes <marco.paulo.lopes@gmail.com>
2021-08-09 15:25:26 +0100

    Improve k8s env switching

--- .config/fish/functions/gcloud-env.fish
+++ .config/fish/functions/gcloud-env.fish
@@ -1,3 +1,9 @@
 function gcloud-env
-  gcloud --project $argv[1] container clusters get-credentials paygplus --region europe-west1;
+  if test "$argv[1]" = "smile"
+    gcloud --project $argv[2] container clusters get-credentials turbo --region europe-west1;
+  else if test "$argv[1]" = "boost-nonprod-198211"
+    gcloud --project $argv[1] container clusters get-credentials vibe-nonprod-billy --region europe-west1
+  else if test "$argv[1]" = "boost-prod"
+    gcloud --project $argv[1] container clusters get-credentials vibe-prod-billy --region europe-west1
+  end
 end
